110d0d93a35351bf6fd56e608c6cd99e92ab9291
Aleksi <aleksi.saxlund@gmail.com>
2023-11-06 11:24:45 +0200

    asd

--- src/varasto.py
+++ src/varasto.py
@@ -2,6 +2,7 @@ class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
+            #asd
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
